065a485e1e2152ab939b9d2569eaf40c9ff9f3b8
ocboogie <boogie@mikulec.com>
2018-10-07 18:17:51 -0700

    Add transitions to posts

--- src/client/components/Posts.vue
+++ src/client/components/Posts.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="posts">
-    <Post 
-      v-for="post in posts" 
-      :key="post.id" 
-      v-bind="post"
-    />
+    <transition-group name="post-list">
+      <Post 
+        v-for="post in posts" 
+        :key="post.id" 
+        v-bind="post"
+      />
+    </transition-group>
   </div>
 </template>
 <script>
-import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 import Post from "./Post.vue";
 
 export default {
   components: {
     Post
   },
-  computed: mapState({
-    posts: "posts"
+  computed: mapGetters({
+    posts: "sortedPosts"
   })
 };
 </script>
 
 <style lang="scss" scoped>
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.25s ease-out;
+}
+.post-list-enter,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.post-list-move {
+  transition: transform 0.25s;
+}
 </style>
