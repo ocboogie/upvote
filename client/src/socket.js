35f7ff1ab43d9dff1102fbe4eb1750871a1a617d
ocboogie <boogie@mikulec.com>
2018-10-25 01:56:04 -0700

    Make client emit function more functional

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,5 @@
 const ws = new WebSocket(process.env.VUE_APP_API_URI);
 
-export function emit(type, data) {
-  ws.send(JSON.stringify({ type, data }));
-}
+export const emit = (type, data) => ws.send(JSON.stringify({ type, data }));
 
 export default ws;
