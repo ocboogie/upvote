decb5f8d7730c9b74c33c1b083a489ccc17f1368
ocboogie <boogie@mikulec.com>
2018-10-28 21:55:55 -0700

    Add a filter to broadcast

--- server/src/wss.js
+++ server/src/wss.js
@@ -15,9 +15,13 @@ export const messageCreator = (type, payload) =>
 export const emit = (who, type, payload) =>
   who.send(messageCreator(type, payload));
 
-export const broadcast = (type, payload) =>
+export const broadcast = (type, payload, filter) =>
   wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN && client.id) {
+    if (
+      client.readyState === WebSocket.OPEN &&
+      client.id &&
+      (!filter || filter(client))
+    ) {
       client.send(messageCreator(type, payload));
     }
   });
