86a58630dbb65877b79307fc5193f2ade5a3d443
ocboogie <boogie@mikulec.com>
2018-12-15 21:29:04 -0800

    Attempt to fix server client count down desync #7

--- server/src/typings.d.ts
+++ server/src/typings.d.ts
@@ -17,7 +17,7 @@ declare global {
       posts: PostForClient[]
       players: string[]
       prompt: string
-      roundEndAt: Date
+      roundEndAt: number
     }
     joinedLobby: {
       players: string[]
