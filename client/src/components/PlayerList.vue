3ef3c42719a157fbb7822ef2194b9dd3a0335103
ocboogie <boogie@mikulec.com>
2019-02-12 19:13:34 -0800

    Add avatars to the player list

--- client/src/components/PlayerList.vue
+++ client/src/components/PlayerList.vue
@@ -1,18 +1,24 @@
 <template>
-  <base-card class="player-list">
+  <div class="player-list">
     <h3 class="header">Players</h3>
     <!-- TODO: Add a group transition -->
     <ul class="list">
-      <li v-for="{ name, id } in players" :key="id" class="player">
-        {{ name }} <span v-if="id === playerId" class="you-label"> (you)</span>
+      <li v-for="{ name, avatar, id } in players" :key="id" class="player">
+        <avatar class="avatar" :avatar-data="avatar" />
+        <div class="name">{{ name }}</div>
+        <div v-if="id === playerId" class="you-label">&nbsp;(you)</div>
       </li>
     </ul>
-  </base-card>
+  </div>
 </template>
 <script>
 import { mapState } from "vuex"
+import Avatar from "./Avatar.vue"
 
 export default {
+  components: {
+    Avatar
+  },
   computed: {
     ...mapState({
       players: state => state.lobby.players,
@@ -23,18 +29,43 @@ export default {
 </script>
 <style lang="scss" scoped>
 .player-list {
+  max-width: 250px;
   .header {
     margin-top: 0;
-    margin-bottom: 0.3rem;
+    margin-bottom: 0.5rem;
   }
   .list {
-    line-height: 1.35;
+    line-height: 3.35;
     list-style-type: none;
-    padding-left: 0.5rem;
+    padding-left: 0rem;
     margin-top: 0;
     margin-bottom: 0;
-    .you-label {
-      color: rgba(0, 0, 0, 0.6);
+    .player {
+      // height: 33px;
+      height: calc(25px + 0.5rem);
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+      &:not(:last-child) {
+        padding-bottom: 0.5rem;
+        margin-bottom: 0.5rem;
+        border-bottom: 1px solid $border-lighter-color;
+      }
+      .avatar {
+        height: 25px;
+        width: 25px;
+        margin-right: 5px;
+        flex-shrink: 0;
+      }
+      .name {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+      .you-label {
+        color: rgba(0, 0, 0, 0.6);
+      }
     }
   }
 }
