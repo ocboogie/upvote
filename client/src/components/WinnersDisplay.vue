c1cfd25fa25924e2d7cace9f06e7016572ea6cf4
ocboogie <boogie@mikulec.com>
2019-03-13 05:24:15 -0700

    Add avatars to Winner Display

--- client/src/components/WinnersDisplay.vue
+++ client/src/components/WinnersDisplay.vue
@@ -1,17 +1,24 @@
 <template>
   <modal v-if="winners" type="unclosable" under-page-transition>
     <h2 class="title">{{ winnerText }}</h2>
-    <div v-for="winner in winners" :key="winner.id">{{ winner.name }}</div>
+    <div class="winners">
+      <div v-for="winner in winners" :key="winner.id" class="winner">
+        <avatar class="avatar" :avatar-data="winner.avatar" rainbow />
+        <div class="name">{{ winner.name }}</div>
+      </div>
+    </div>
   </modal>
 </template>
 
 <script>
 import { mapGetters } from "vuex"
 import Modal from "./Modal.vue"
+import Avatar from "./Avatar.vue"
 
 export default {
   components: {
-    Modal
+    Modal,
+    Avatar
   },
   computed: {
     ...mapGetters(["winners"]),
@@ -30,5 +37,20 @@ export default {
 <style lang="scss" scoped>
 .title {
   margin-top: 0;
+}
+.winners {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  .winner {
+    padding: 0.5rem;
+    margin-bottom: 0.5rem;
+    .avatar {
+      width: 150px;
+      border: 1px solid $border-lighter-color;
+    }
+  }
 }
 </style>
